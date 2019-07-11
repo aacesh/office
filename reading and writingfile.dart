c772671c5daf1b2766ce3aff7167f101c45d17b1
aacesh <52647571+aacesh@users.noreply.github.com>
2019-07-11 03:40:40 -0700

    Add files via upload

---
+++ reading and writingfile.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+import 'dart:async';
+
+import 'package:path_provider/path_provider.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Reading and Writing to Storage",
+      home: Home(
+        storage: Storage(),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  final Storage storage;
+
+  Home({Key key, @required this.storage}) : super(key: key);
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  TextEditingController controller = TextEditingController();
+  String state;
+  Future<Directory> _appDocDir;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readData().then((String value) {
+      setState(() {
+        state = value;
+      });
+    });
+  }
+
+  Future<File> writeData() async {
+    setState(() {
+      state = controller.text;
+      controller.text = '';
+    });
+
+    return widget.storage.writeData(state);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reading and Writing Files'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('${state ?? "File is Empty"}'),
+            TextField(
+              controller: controller,
+            ),
+            RaisedButton(
+              onPressed: writeData,
+              child: Text('Write to File'),
+            ),
+          
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Storage {
+  Future<String> get localPath async {
+    final dir = await getApplicationDocumentsDirectory();
+    return dir.path;
+  }
+
+  Future<File> get localFile async {
+    final path = await localPath;
+    return File('$path/db.txt');
+  }
+
+  Future<String> readData() async {
+    try {
+      final file = await localFile;
+      String body = await file.readAsString();
+
+      return body;
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<File> writeData(String data) async {
+    final file = await localFile;
+    return file.writeAsString("$data");
+  }
+}
