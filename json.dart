f593cb0114259b8656b9d1887e53db838d7335b4
aacesh <52647571+aacesh@users.noreply.github.com>
2019-07-24 16:53:51 +0545

    Add files via upload

--- json.dart
+++ json.dart
@@ -1,147 +1,83 @@
 
-
-
-main() {
+void main() {
+  
   PlanningFormModel pfm = PlanningFormModel();
-  print(pfm.toString());
+  CostElements ce = new CostElements();
+ Transportation t =new Transportation();
+  ce.transportation = [t];
+  pfm.costElements = [ce];
   print(pfm.toJson());
+    
+  
 }
-  
-  
-  class PlanningFormModel{
-    String Company;
-    String Department;
-    List<String> costElements;
-    Map<String, MonthlyPlan> ceToMpMap;
-     List<int> amtList;
-     MonthlyPlan mPlan = new MonthlyPlan();
-    String allCatagory;
-    
-    PlanningFormModel(){
-      this.Company = "N Tech";
-      this.Department = "Marketing";
-      this.costElements = new List<String>();
-    
-      this.costElements.add("Transportation");
-      this.costElements.add("Marketing");
-      this.costElements.add("Human Resources");
-      this.costElements.add("Information Technology");
-      this.costElements.add("Legal");
-    
-      //instatiate the map to store monthly plan for each costEleemnts
-      ceToMpMap = new Map<String,MonthlyPlan>();
-     
-     
-     
+class PlanningFormModel {
+  String company;
+  String department;
+  List<CostElements> costElements;
 
+  PlanningFormModel({this.company, this.department, this.costElements});
 
-       //mp = mp.getbykey(legal)
-     //  mp=mp.amountInMonth
-
-      for(String ce in this.costElements){
-        //creaete monthly plan for each cost element 
-       
-        mPlan.category = ce;
-       amtList = new List<int>();
-      
-        for (int i = 1; i < 13; i++) {
-          //assign some amount to each of the 12 months
-          amtList.add(i*125);
-        }
-        mPlan.amountInMonth = amtList;
-      
-        List hr = new List<int>();
-      
-        for (int i = 1; i < 13; i++) {
-          //assign some amount to each of the 12 months
-          hr.add(i*7);
-        }
-        mPlan.hourInMonth = hr;
-
-        //add monthly plan for the is 
-     this.ceToMpMap[ce] = mPlan;
-        
-        
-      }
-      //assign month plan
-     // this.ceToMpMap = ceToMpM;
-      
-      
-
-      
+  PlanningFormModel.fromJson(Map<String, dynamic> json) {
+    company = json['Company'];
+    department = json['Department'];
+    if (json['costElements'] != null) {
+      costElements = new List<CostElements>();
+      json['costElements'].forEach((v) {
+        costElements.add(new CostElements.fromJson(v));
+      });
     }
-    
-    
-    
-    String toString(){
-      String planningFormInString = "Company = "+this.Company+"\n"+"DepartMent = "+this.Department+"\n"; 
-      
-      planningFormInString = planningFormInString+"Monthly Amount Plan\n";
-      for(String ce in this.costElements){
-        planningFormInString = planningFormInString+ce+" ";
-        
-        MonthlyPlan mp = this.ceToMpMap[ce];
+  }
 
-        for(int amount in mp.amountInMonth){
-          planningFormInString = planningFormInString+amount.toString()+"||";
-          
-        }
-        
-        planningFormInString = planningFormInString+"\n";
-          
-      }
-      
-      planningFormInString = planningFormInString+"Monthly hour Plan\n";
-      for(String ce in this.costElements){
-        planningFormInString = planningFormInString+ce+" ";
-        
-        MonthlyPlan mp = this.ceToMpMap[ce];
-        
-
-        for(int hour in mp.hourInMonth){
-          planningFormInString = planningFormInString+hour.toString()+"||";
-          
-        }
-        planningFormInString = planningFormInString+"\n";
-
-       
-      }
-      return planningFormInString;
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['Company'] = this.company;
+    data['Department'] = this.department;
+    if (this.costElements != null) {
+      data['costElements'] = this.costElements.map((v) => v.toJson()).toList();
     }
-      
-    
-    
-    toJson(){
-      
-          var pmfToJson = { "company" : "${this.Company}","department":"${this.Department}"};
-      
-         for(String ce in this.costElements){
-         String pmfInString="";
-        
-        MonthlyPlan mp = this.ceToMpMap[ce];
-
-        for(int amount in mp.amountInMonth){
-          pmfInString = pmfInString+amount.toString()+"||";
-          
-        }
-        
-        pmfInString = pmfInString+"\n";
-        
-    
- 
+    return data;
+  }
 }
-           var jsonDataString = "{'pmfInString'}";
-        var jsonData =json.parse(jsonDataString);
-        
-       pmfToJson = pmfToJson.concat(jsonData);
-  return pmfToJson;
-  
-  }
+
+class CostElements {
+  List<Transportation> transportation;
+
+  CostElements({this.transportation});
+
+  CostElements.fromJson(Map<String, dynamic> json) {
+    if (json['Transportation'] != null) {
+      transportation = new List<Transportation>();
+      json['Transportation'].forEach((v) {
+        transportation.add(new Transportation.fromJson(v));
+      });
+    }
   }
 
-
-  class MonthlyPlan{
-    String category;
-    List<int> amountInMonth;
-    List<int> hourInMonth;
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.transportation != null) {
+      data['Transportation'] =
+          this.transportation.map((v) => v.toJson()).toList();
+    }
+    return data;
   }
+}
+
+class Transportation {
+  int amountInMonth;
+  int hourInMonth;
+
+  Transportation({this.amountInMonth, this.hourInMonth});
+
+  Transportation.fromJson(Map<String, dynamic> json) {
+    amountInMonth = json['amountInMonth'];
+    hourInMonth = json['hourInMonth'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['amountInMonth'] = this.amountInMonth;
+    data['hourInMonth'] = this.hourInMonth;
+    return data;
+  }
+}
